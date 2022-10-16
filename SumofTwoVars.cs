294ef8de91ee6f0f7da0315681886ebc7d41542b
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-16 23:21:49 +0100

    Add files via upload

---
+++ SumofTwoVars.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main() {
+        int x = 10;
+        int y = 25;
+        int z = x + y;
+
+        Console.Write("Sum of x + y = "+ z);
+    }
+}
