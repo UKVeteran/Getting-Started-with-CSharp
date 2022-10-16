7701a7e492ce7fde9be665f4e239d57951d03cdf
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-16 23:26:54 +0100

    Add files via upload

---
+++ Strings/CreateString.cs
@@ -0,0 +1,17 @@
+using System;  
+namespace CsharpString {  
+  class Test {
+    public static void Main(string [] args) {
+      
+      // create string
+      string str1 = "C# Programming";
+      string str2 = "Programiz";
+      
+      // print string
+      Console.WriteLine(str1);
+      Console.WriteLine(str2);
+
+      Console.ReadLine();
+    }
+  } 
+}
