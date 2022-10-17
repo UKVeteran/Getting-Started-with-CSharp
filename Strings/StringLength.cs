8a317c6c6559d401ad3910d8c487bdb64f668a48
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-17 07:20:48 +0100

    Add files via upload

---
+++ Strings/StringLength.cs
@@ -0,0 +1,17 @@
+using System;  
+namespace CsharpString {  
+  class Test {
+    public static void Main(string [] args) {
+
+      // create string
+      string str = "C# Programming";
+      Console.WriteLine("string: " + str);
+      
+      // get length of str
+      int length = str.Length;     
+      Console.WriteLine("Length: "+ length);
+
+      Console.ReadLine();
+    }
+  } 
+}
