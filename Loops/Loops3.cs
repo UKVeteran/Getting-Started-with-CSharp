c60cf51c531d90fbf68cc70679fd3ef3d1dc62e1
Johar M. Ashfaque, Ph.D., MInstP, AATQB <39216339+UKVeteran@users.noreply.github.com>
2022-10-18 06:17:25 +0100

    Add files via upload

---
+++ Loops/Loops3.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Loop
+{
+	class WhileLoop
+	{
+		public static void Main(string[] args)
+		{
+			int i=1, sum=0;
+
+			while (i<=5)
+			{
+				sum += i;
+				i++;
+			}
+			Console.WriteLine("Sum = {0}", sum);
+		}
+	}
+}
